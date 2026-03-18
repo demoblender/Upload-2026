2372d92785c1683819ea711ee3b6751e02abe74b
demoblender <demoblenderarts@gmail.com>
2026-03-17 21:52:04 -0400

    demo

--- demo/types.d.ts
+++ demo/types.d.ts
@@ -1,23 +1,23 @@
 /**
- * @fileoverview Types for the config-array package.
- * @author Nicholas C. Zakas
+ * @fileoverview Types for this package.
  */
-export interface ConfigObject {
-    /**
-     * The base path for files and ignores.
-     */
-    basePath?: string;
-    /**
-     * The files to include.
-     */
-    files?: string[];
-    /**
-     * The files to exclude.
-     */
-    ignores?: string[];
-    /**
-     * The name of the config object.
-     */
-    name?: string;
-    [key: string]: unknown;
+import type { ConfigObject } from "@eslint/core";
+/**
+ * Infinite array type.
+ */
+export type InfiniteArray<T> = T | InfiniteArray<T>[];
+/**
+ * The type of array element in the `extends` property after flattening.
+ */
+export type SimpleExtendsElement = string | ConfigObject;
+/**
+ * The type of array element in the `extends` property before flattening.
+ */
+export type ExtendsElement = SimpleExtendsElement | InfiniteArray<ConfigObject>;
+/**
+ * Config with extends. Valid only inside of `defineConfig()`.
+ */
+export interface ConfigWithExtends extends ConfigObject {
+    extends?: ExtendsElement[];
 }
+export type ConfigWithExtendsArray = InfiniteArray<ConfigWithExtends>[];
