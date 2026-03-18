387dd5b11e5a74e8cc31dbeda14b7ed7d161f178
demoblender <demoblenderarts@gmail.com>
2026-03-17 21:52:50 -0400

    demo

--- demo/index.d.ts
+++ demo/index.d.ts
@@ -1,145 +1,217 @@
-export { ObjectSchema } from "@eslint/object-schema";
-export type PropertyDefinition = $eslintobjectschema.PropertyDefinition;
-export type ObjectDefinition = $eslintobjectschema.ObjectDefinition;
-export type ConfigObject = $typests.ConfigObject;
-export type IMinimatchStatic = minimatch.IMinimatchStatic;
-export type IMinimatch = minimatch.IMinimatch;
-export type ObjectSchemaInstance = ObjectSchema;
-/**
- * Represents an array of config objects and provides method for working with
- * those config objects.
- */
-export class ConfigArray extends Array<any> {
-    /**
-     * Creates a new instance of ConfigArray.
-     * @param {Iterable|Function|Object} configs An iterable yielding config
-     *      objects, or a config function, or a config object.
-     * @param {Object} options The options for the ConfigArray.
-     * @param {string} [options.basePath="/"] The absolute path of the config file directory.
-     * 		Defaults to `"/"`.
-     * @param {boolean} [options.normalized=false] Flag indicating if the
-     *      configs have already been normalized.
-     * @param {Object} [options.schema] The additional schema
-     *      definitions to use for the ConfigArray schema.
-     * @param {Array<string>} [options.extraConfigTypes] List of config types supported.
-     * @throws {TypeError} When the `basePath` is not a non-empty string,
-     */
-    constructor(configs: Iterable<any> | Function | any, { basePath, normalized, schema: customSchema, extraConfigTypes, }?: {
-        basePath?: string;
-        normalized?: boolean;
-        schema?: any;
-        extraConfigTypes?: Array<string>;
-    });
-    /**
-     * The path of the config file that this array was loaded from.
-     * This is used to calculate filename matches.
-     * @property basePath
-     * @type {string}
-     */
-    basePath: string;
-    /**
-     * The supported config types.
-     * @type {Array<string>}
-     */
-    extraConfigTypes: Array<string>;
-    /**
-     * Returns the `files` globs from every config object in the array.
-     * This can be used to determine which files will be matched by a
-     * config array or to use as a glob pattern when no patterns are provided
-     * for a command line interface.
-     * @returns {Array<string|Function>} An array of matchers.
-     */
-    get files(): Array<string | Function>;
-    /**
-     * Returns ignore matchers that should always be ignored regardless of
-     * the matching `files` fields in any configs. This is necessary to mimic
-     * the behavior of things like .gitignore and .eslintignore, allowing a
-     * globbing operation to be faster.
-     * @returns {Object[]} An array of config objects representing global ignores.
-     */
-    get ignores(): any[];
-    /**
-     * Indicates if the config array has been normalized.
-     * @returns {boolean} True if the config array is normalized, false if not.
-     */
-    isNormalized(): boolean;
-    /**
-     * Normalizes a config array by flattening embedded arrays and executing
-     * config functions.
-     * @param {Object} [context] The context object for config functions.
-     * @returns {Promise<ConfigArray>} The current ConfigArray instance.
-     */
-    normalize(context?: any): Promise<ConfigArray>;
-    /**
-     * Normalizes a config array by flattening embedded arrays and executing
-     * config functions.
-     * @param {Object} [context] The context object for config functions.
-     * @returns {ConfigArray} The current ConfigArray instance.
-     */
-    normalizeSync(context?: any): ConfigArray;
-    /**
-     * Returns the config object for a given file path and a status that can be used to determine why a file has no config.
-     * @param {string} filePath The path of a file to get a config for.
-     * @returns {{ config?: Object, status: "ignored"|"external"|"unconfigured"|"matched" }}
-     * An object with an optional property `config` and property `status`.
-     * `config` is the config object for the specified file as returned by {@linkcode ConfigArray.getConfig},
-     * `status` a is one of the constants returned by {@linkcode ConfigArray.getConfigStatus}.
-     */
-    getConfigWithStatus(filePath: string): {
-        config?: any;
-        status: "ignored" | "external" | "unconfigured" | "matched";
-    };
-    /**
-     * Returns the config object for a given file path.
-     * @param {string} filePath The path of a file to get a config for.
-     * @returns {Object|undefined} The config object for this file or `undefined`.
-     */
-    getConfig(filePath: string): any | undefined;
-    /**
-     * Determines whether a file has a config or why it doesn't.
-     * @param {string} filePath The path of the file to check.
-     * @returns {"ignored"|"external"|"unconfigured"|"matched"} One of the following values:
-     * * `"ignored"`: the file is ignored
-     * * `"external"`: the file is outside the base path
-     * * `"unconfigured"`: the file is not matched by any config
-     * * `"matched"`: the file has a matching config
-     */
-    getConfigStatus(filePath: string): "ignored" | "external" | "unconfigured" | "matched";
-    /**
-     * Determines if the given filepath is ignored based on the configs.
-     * @param {string} filePath The path of a file to check.
-     * @returns {boolean} True if the path is ignored, false if not.
-     * @deprecated Use `isFileIgnored` instead.
-     */
-    isIgnored(filePath: string): boolean;
-    /**
-     * Determines if the given filepath is ignored based on the configs.
-     * @param {string} filePath The path of a file to check.
-     * @returns {boolean} True if the path is ignored, false if not.
-     */
-    isFileIgnored(filePath: string): boolean;
-    /**
-     * Determines if the given directory is ignored based on the configs.
-     * This checks only default `ignores` that don't have `files` in the
-     * same config. A pattern such as `/foo` be considered to ignore the directory
-     * while a pattern such as `/foo/**` is not considered to ignore the
-     * directory because it is matching files.
-     * @param {string} directoryPath The path of a directory to check.
-     * @returns {boolean} True if the directory is ignored, false if not. Will
-     * 		return true for any directory that is not inside of `basePath`.
-     * @throws {Error} When the `ConfigArray` is not normalized.
-     */
-    isDirectoryIgnored(directoryPath: string): boolean;
-    #private;
+import * as eslint from 'eslint';
+import { Rule, AST } from 'eslint';
+import * as estree from 'estree';
+
+declare const READ: unique symbol;
+declare const CALL: unique symbol;
+declare const CONSTRUCT: unique symbol;
+declare const ESM: unique symbol;
+declare class ReferenceTracker {
+    constructor(globalScope: Scope$2, options?: {
+        mode?: "legacy" | "strict" | undefined;
+        globalObjectNames?: string[] | undefined;
+    } | undefined);
+    private variableStack;
+    private globalScope;
+    private mode;
+    private globalObjectNames;
+    iterateGlobalReferences<T>(traceMap: TraceMap$2<T>): IterableIterator<TrackedReferences$2<T>>;
+    iterateCjsReferences<T_1>(traceMap: TraceMap$2<T_1>): IterableIterator<TrackedReferences$2<T_1>>;
+    iterateEsmReferences<T_2>(traceMap: TraceMap$2<T_2>): IterableIterator<TrackedReferences$2<T_2>>;
+    iteratePropertyReferences<T_3>(node: Expression, traceMap: TraceMap$2<T_3>): IterableIterator<TrackedReferences$2<T_3>>;
+    private _iterateVariableReferences;
+    private _iteratePropertyReferences;
+    private _iterateLhsReferences;
+    private _iterateImportReferences;
 }
-export namespace ConfigArraySymbol {
-    let isNormalized: symbol;
-    let configCache: symbol;
-    let schema: symbol;
-    let finalizeConfig: symbol;
-    let preprocessConfig: symbol;
+declare namespace ReferenceTracker {
+    export { READ };
+    export { CALL };
+    export { CONSTRUCT };
+    export { ESM };
 }
-import type * as $eslintobjectschema from "@eslint/object-schema";
-import type * as $typests from "./types.ts";
-import minimatch from 'minimatch';
-import { ObjectSchema } from '@eslint/object-schema';
+type Scope$2 = eslint.Scope.Scope;
+type Expression = estree.Expression;
+type TraceMap$2<T> = TraceMap$1<T>;
+type TrackedReferences$2<T> = TrackedReferences$1<T>;
+
+type StaticValue$2 = StaticValueProvided$1 | StaticValueOptional$1;
+type StaticValueProvided$1 = {
+    optional?: undefined;
+    value: unknown;
+};
+type StaticValueOptional$1 = {
+    optional?: true;
+    value: undefined;
+};
+type ReferenceTrackerOptions$1 = {
+    globalObjectNames?: string[];
+    mode?: "legacy" | "strict";
+};
+type TraceMap$1<T = unknown> = {
+    [i: string]: TraceMapObject<T>;
+};
+type TraceMapObject<T> = {
+    [i: string]: TraceMapObject<T>;
+    [CALL]?: T;
+    [CONSTRUCT]?: T;
+    [READ]?: T;
+    [ESM]?: boolean;
+};
+type TrackedReferences$1<T> = {
+    info: T;
+    node: Rule.Node;
+    path: string[];
+    type: typeof CALL | typeof CONSTRUCT | typeof READ;
+};
+type HasSideEffectOptions$1 = {
+    considerGetters?: boolean;
+    considerImplicitTypeConversion?: boolean;
+};
+type PunctuatorToken<Value extends string> = AST.Token & {
+    type: "Punctuator";
+    value: Value;
+};
+type ArrowToken$1 = PunctuatorToken<"=>">;
+type CommaToken$1 = PunctuatorToken<",">;
+type SemicolonToken$1 = PunctuatorToken<";">;
+type ColonToken$1 = PunctuatorToken<":">;
+type OpeningParenToken$1 = PunctuatorToken<"(">;
+type ClosingParenToken$1 = PunctuatorToken<")">;
+type OpeningBracketToken$1 = PunctuatorToken<"[">;
+type ClosingBracketToken$1 = PunctuatorToken<"]">;
+type OpeningBraceToken$1 = PunctuatorToken<"{">;
+type ClosingBraceToken$1 = PunctuatorToken<"}">;
+
+declare function findVariable(initialScope: Scope$1, nameOrNode: string | Identifier): Variable | null;
+type Scope$1 = eslint.Scope.Scope;
+type Variable = eslint.Scope.Variable;
+type Identifier = estree.Identifier;
+
+declare function getFunctionHeadLocation(node: FunctionNode$1, sourceCode: SourceCode$2): SourceLocation | null;
+type SourceCode$2 = eslint.SourceCode;
+type FunctionNode$1 = estree.Function;
+type SourceLocation = estree.SourceLocation;
+
+declare function getFunctionNameWithKind(node: FunctionNode, sourceCode?: eslint.SourceCode | undefined): string;
+type FunctionNode = estree.Function;
+
+declare function getInnermostScope(initialScope: Scope, node: Node$4): Scope;
+type Scope = eslint.Scope.Scope;
+type Node$4 = estree.Node;
+
+declare function getPropertyName(node: MemberExpression | MethodDefinition | Property | PropertyDefinition, initialScope?: eslint.Scope.Scope | undefined): string | null | undefined;
+type MemberExpression = estree.MemberExpression;
+type MethodDefinition = estree.MethodDefinition;
+type Property = estree.Property;
+type PropertyDefinition = estree.PropertyDefinition;
+
+declare function getStaticValue(node: Node$3, initialScope?: eslint.Scope.Scope | null | undefined): StaticValue$1 | null;
+type StaticValue$1 = StaticValue$2;
+type Node$3 = estree.Node;
+
+declare function getStringIfConstant(node: Node$2, initialScope?: eslint.Scope.Scope | null | undefined): string | null;
+type Node$2 = estree.Node;
+
+declare function hasSideEffect(node: Node$1, sourceCode: SourceCode$1, options?: HasSideEffectOptions$1 | undefined): boolean;
+type Node$1 = estree.Node;
+type SourceCode$1 = eslint.SourceCode;
+
+declare function isArrowToken(token: CommentOrToken): token is ArrowToken$1;
+declare function isCommaToken(token: CommentOrToken): token is CommaToken$1;
+declare function isSemicolonToken(token: CommentOrToken): token is SemicolonToken$1;
+declare function isColonToken(token: CommentOrToken): token is ColonToken$1;
+declare function isOpeningParenToken(token: CommentOrToken): token is OpeningParenToken$1;
+declare function isClosingParenToken(token: CommentOrToken): token is ClosingParenToken$1;
+declare function isOpeningBracketToken(token: CommentOrToken): token is OpeningBracketToken$1;
+declare function isClosingBracketToken(token: CommentOrToken): token is ClosingBracketToken$1;
+declare function isOpeningBraceToken(token: CommentOrToken): token is OpeningBraceToken$1;
+declare function isClosingBraceToken(token: CommentOrToken): token is ClosingBraceToken$1;
+declare function isCommentToken(token: CommentOrToken): token is estree.Comment;
+declare function isNotArrowToken(arg0: CommentOrToken): boolean;
+declare function isNotCommaToken(arg0: CommentOrToken): boolean;
+declare function isNotSemicolonToken(arg0: CommentOrToken): boolean;
+declare function isNotColonToken(arg0: CommentOrToken): boolean;
+declare function isNotOpeningParenToken(arg0: CommentOrToken): boolean;
+declare function isNotClosingParenToken(arg0: CommentOrToken): boolean;
+declare function isNotOpeningBracketToken(arg0: CommentOrToken): boolean;
+declare function isNotClosingBracketToken(arg0: CommentOrToken): boolean;
+declare function isNotOpeningBraceToken(arg0: CommentOrToken): boolean;
+declare function isNotClosingBraceToken(arg0: CommentOrToken): boolean;
+declare function isNotCommentToken(arg0: CommentOrToken): boolean;
+type Token = eslint.AST.Token;
+type Comment = estree.Comment;
+type CommentOrToken = Comment | Token;
+
+declare function isParenthesized(timesOrNode: Node | number, nodeOrSourceCode: Node | SourceCode, optionalSourceCode?: eslint.SourceCode | undefined): boolean;
+type Node = estree.Node;
+type SourceCode = eslint.SourceCode;
+
+declare class PatternMatcher {
+    constructor(pattern: RegExp, options?: {
+        escaped?: boolean | undefined;
+    } | undefined);
+    execAll(str: string): IterableIterator<RegExpExecArray>;
+    test(str: string): boolean;
+    [Symbol.replace](str: string, replacer: string | ((...strs: string[]) => string)): string;
+}
+
+declare namespace _default {
+    export { CALL };
+    export { CONSTRUCT };
+    export { ESM };
+    export { findVariable };
+    export { getFunctionHeadLocation };
+    export { getFunctionNameWithKind };
+    export { getInnermostScope };
+    export { getPropertyName };
+    export { getStaticValue };
+    export { getStringIfConstant };
+    export { hasSideEffect };
+    export { isArrowToken };
+    export { isClosingBraceToken };
+    export { isClosingBracketToken };
+    export { isClosingParenToken };
+    export { isColonToken };
+    export { isCommaToken };
+    export { isCommentToken };
+    export { isNotArrowToken };
+    export { isNotClosingBraceToken };
+    export { isNotClosingBracketToken };
+    export { isNotClosingParenToken };
+    export { isNotColonToken };
+    export { isNotCommaToken };
+    export { isNotCommentToken };
+    export { isNotOpeningBraceToken };
+    export { isNotOpeningBracketToken };
+    export { isNotOpeningParenToken };
+    export { isNotSemicolonToken };
+    export { isOpeningBraceToken };
+    export { isOpeningBracketToken };
+    export { isOpeningParenToken };
+    export { isParenthesized };
+    export { isSemicolonToken };
+    export { PatternMatcher };
+    export { READ };
+    export { ReferenceTracker };
+}
+
+type StaticValue = StaticValue$2;
+type StaticValueOptional = StaticValueOptional$1;
+type StaticValueProvided = StaticValueProvided$1;
+type ReferenceTrackerOptions = ReferenceTrackerOptions$1;
+type TraceMap<T> = TraceMap$1<T>;
+type TrackedReferences<T> = TrackedReferences$1<T>;
+type HasSideEffectOptions = HasSideEffectOptions$1;
+type ArrowToken = ArrowToken$1;
+type CommaToken = CommaToken$1;
+type SemicolonToken = SemicolonToken$1;
+type ColonToken = ColonToken$1;
+type OpeningParenToken = OpeningParenToken$1;
+type ClosingParenToken = ClosingParenToken$1;
+type OpeningBracketToken = OpeningBracketToken$1;
+type ClosingBracketToken = ClosingBracketToken$1;
+type OpeningBraceToken = OpeningBraceToken$1;
+type ClosingBraceToken = ClosingBraceToken$1;
+
+export { ArrowToken, CALL, CONSTRUCT, ClosingBraceToken, ClosingBracketToken, ClosingParenToken, ColonToken, CommaToken, ESM, HasSideEffectOptions, OpeningBraceToken, OpeningBracketToken, OpeningParenToken, PatternMatcher, READ, ReferenceTracker, ReferenceTrackerOptions, SemicolonToken, StaticValue, StaticValueOptional, StaticValueProvided, TraceMap, TrackedReferences, _default as default, findVariable, getFunctionHeadLocation, getFunctionNameWithKind, getInnermostScope, getPropertyName, getStaticValue, getStringIfConstant, hasSideEffect, isArrowToken, isClosingBraceToken, isClosingBracketToken, isClosingParenToken, isColonToken, isCommaToken, isCommentToken, isNotArrowToken, isNotClosingBraceToken, isNotClosingBracketToken, isNotClosingParenToken, isNotColonToken, isNotCommaToken, isNotCommentToken, isNotOpeningBraceToken, isNotOpeningBracketToken, isNotOpeningParenToken, isNotSemicolonToken, isOpeningBraceToken, isOpeningBracketToken, isOpeningParenToken, isParenthesized, isSemicolonToken };
